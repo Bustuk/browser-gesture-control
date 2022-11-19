65b17b920d918ef8bdcf4155600d798f18ecf090
Łukasz Wołodkiewicz <wolodkiewiczlukasz@gmail.com>
2022-11-19 20:51:35 +0100

    Fix content script injecting in dev mode

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,8 @@
 import type { Tabs } from 'webextension-polyfill'
 import browser from 'webextension-polyfill'
 import { onMessage, sendMessage } from 'webext-bridge'
-
+if (__DEV__)
+  import('./contentScriptHMR')
 browser.runtime.onInstalled.addListener((): void => {
   // eslint-disable-next-line no-console
   console.log('Extension installed')
@@ -11,7 +12,7 @@ let previousTabId = 0
 
 // communication example: send previous tab title from background page
 // see shim.d.ts for type declaration
-browser.tabs.onActivated.addListener(async({ tabId }) => {
+browser.tabs.onActivated.addListener(async ({ tabId }) => {
   if (!previousTabId) {
     previousTabId = tabId
     return
@@ -32,7 +33,7 @@ browser.tabs.onActivated.addListener(async({ tabId }) => {
   sendMessage('tab-prev', { title: tab.title }, { context: 'content-script', tabId })
 })
 
-onMessage('get-current-tab', async() => {
+onMessage('get-current-tab', async () => {
   try {
     const tab = await browser.tabs.get(previousTabId)
     return {
